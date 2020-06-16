94e985e910a63ad11af5949faf5df954b0f16591
Ankitsen1234 <63501761+Ankitsen1234@users.noreply.github.com>
2020-06-16 21:01:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var ball,database,position;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballPositionref = database.ref('ball/position');
+    ballPositionref.on("value",readPositon,showError)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+   database.ref('ball/position').set({
+       'x':position.x+x,
+       'y':position.y+y
+   })
+}
+function readPositon(data){
+    position = data.val()
+    console.log(position)
+    ball.x=position.x;
+    ball.y=position.y
+}
+function showError(){
+    console.log("error")
+}
